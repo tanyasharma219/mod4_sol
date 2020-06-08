ff0798b8e0a05e1c99af02767d9e9a51e3e4896d
tanyasharma219 <63143037+tanyasharma219@users.noreply.github.com>
2020-06-08 23:44:01 +0530

    Update hello.js

--- hello.js
+++ hello.js
@@ -1,4 +1,8 @@
-var speakWord = "Hello";
-function speak(name) {
-  console.log(speakWord + " " + name);
-}
+(function(window) {
+    var helloSpeaker = new Object();
+    var speakWord = "Hello";
+    helloSpeaker.speak = function(name) {
+        console.log(speakWord + " " + name);
+    };
+    window.helloSpeaker = helloSpeaker;
+})(window);
