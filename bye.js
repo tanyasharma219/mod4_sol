76e593d6063b417c52fe0b5aff69aa2d31faf522
tanyasharma219 <63143037+tanyasharma219@users.noreply.github.com>
2020-06-08 23:52:29 +0530

    Update bye.js

--- bye.js
+++ bye.js
@@ -1,9 +1,9 @@
-(function(window) {
-    var byeSpeaker = new Object();
-    var speakWord = "Good Bye";
-    byeSpeaker.speak = function speak(name) {
-        console.log(speakWord + " " + name);
+(function (window) {
+    var speakWord = "Good bye";
+    var byeSpeaker = {
+          speak  :  function (name){
+             console.log(speakWord + " " + name);
+          }
     };
     window.byeSpeaker = byeSpeaker;
-})(window);
-
+ })(window);
